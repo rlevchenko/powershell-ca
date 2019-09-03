e9898198d567c8206e2a0f39724148ea8336c58c
R.LEVCHENKO <c0rte0us@gmail.com>
2019-09-03 16:32:15 +0300

    add files

---
+++ CAWE.cr/CAWE.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Description
+       Installs required server roles for Root CA including Web Enrollment.
+       + very simple test 
+
+.NOTES
+       Name: CA Installation
+       Author : Roman Levchenko
+       WebSite: www.rlevchenko.com
+       Prerequisites: domain infrastructure, defined args
+
+#>
+
+#Variables
+$pass = $args[0] #VMM argument
+$domain = (gwmi WIN32_ComputerSystem).Domain
+$secpass = convertto-securestring $pass -asplaintext -force
+$credential = New-Object System.Management.Automation.PsCredential -ArgumentList "$domain\Administrator", $secpass
+#CA+WE
+Install-ADCSCertificationAuthority -CAType EnterpriseRootCA -Credential $credential -Confirm:$false
+Install-ADcsWebEnrollment -Credential $credential -Force
+#Simple test
+$test = (Get-WebVirtualDirectory).path
+If ($test -eq "/CertEnroll") { "CAWE WAS INSTALLED SUCCESSFULLY" }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# powershell-ca
-PowerShell script to install Certificate Authority
+# PowerShell - Certificate Authority
+PowerShell script to install Root Certificate Authority
+- CAWE.ps1 installs CA and components
+- Domain.ps1 joins a machine to domain
